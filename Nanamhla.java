738b8658e0858b46e780f615a786bf90f13993f3
nanamhlamanyifolo <84190335+nanamhlamanyifolo@users.noreply.github.com>
2021-05-20 18:51:17 +0200

    Add files via upload

---
+++ Nanamhla.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+
+public class Nanamhla {
+	
+	public static void main (String[] args) {
+	JOptionPane.showMessageDialog(null, "Programming is fun");
+	  System.exit(0);
+	}
+
+}
